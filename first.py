971ef201e826beb5600c9dfac761e5407dc8d903
sufiyanshaikh01 <softwaremastar0010@gmail.com>
2026-08-21 19:59:20 +0530

    Add te new file

---
+++ first.py
@@ -0,0 +1,17 @@
+# _________________________________First code_________________________________________
+# marks = int(input("Enter the marks: "))
+
+# if marks <= 100 & marks >= 90:
+#     print('A')
+# elif marks <= 90 & marks >=75:
+#     print('B')
+# elif marks < 75 & marks >=60:
+#     print('C')
+# elif marks < 60 & marks >=40:
+#     print('D')
+# else :
+#     print("F")
+    
+# print("End of code")
+
+    
