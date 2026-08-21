f4a5578b68e0f7534da379ba347c2f8151ca6996
sufiyanshaikh01 <softwaremastar0010@gmail.com>
2026-08-21 20:40:27 +0530

    Update Module file

---
+++ Modules/module.py
@@ -0,0 +1,22 @@
+pi = 3.14
+
+def area(r):
+    return pi * r**2
+
+print("App is Loaded...")
+
+def parameter(l, w):
+    return  2*l + 2*w
+
+print("App is loaded")
+
+def square(a):
+    return a*a
+print("App is loaded")
+
+def abc():
+    xyz = [1, 2, 3, 4, 5, 6, 7]
+    return xyz
+
+if __name__ == "__main__":
+    print("Testing:",)
